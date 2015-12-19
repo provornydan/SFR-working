617ae8266791dfbfcdfd4c73d2cdd1974104ea49
provornydan <provornydan@gmail.com>
2015-12-19 05:46:07 +0200

    vbxw

--- app/src/main/java/mazebug/sfr_working/Jmurik.java
+++
@@ -1,7 +0,0 @@
-package mazebug.sfr_working;
-
-/**
- * Created by Citac Alex on 19.12.2015.
- */
-public class Jmurik {
-}
---
+++ app/src/main/java/mazebug/sfr_working/NewSFR.java
@@ -0,0 +1,13 @@
+package mazebug.sfr_working;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewSFR extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_sfr);
+    }
+}
